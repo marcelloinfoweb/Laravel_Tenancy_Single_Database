48571e4ee7f6e6ef6341470fab1042e7eb20ccb6
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-07-23 16:01:38 -0300

    Serviço para Carrinho

---
+++ app/Service/CartService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Session\SessionManager as Session;
+
+class CartService
+{
+    const CART_KEY = 'cart';
+
+    public function __construct(private Session $session)
+    {
+
+    }
+
+    public function all()
+    {
+        $items = $this->session->get(self::CART_KEY);
+
+        return ! $items ? [] : $items;
+    }
+
+    public function add($item)
+    {
+        if ($this->session->has(self::CART_KEY)) {
+
+            if ($this->itemExistInCart($item)) {
+                throw new \Exception('Product Already Exists in Car!');
+            }
+
+            $this->session->put(self::CART_KEY, $item);
+
+        } else {
+            $this->session->put(self::CART_KEY, [$item]);
+        }
+    }
+
+    public function remove($item)
+    {
+        $items = $this->session->get(self::CART_KEY);
+
+        $items = array_filter($items, function ($line) use ($item) {
+            return $line['slug'] != $item;
+        });
+
+        $this->session->put(self::CART_KEY, $items);
+    }
+
+    public function clear()
+    {
+        $this->session->forget(self::CART_KEY);
+    }
+
+    public function itemExistInCart($item)
+    {
+        $items = $this->session->get(self::CART_KEY);
+
+        $somethingSlugsInCart = array_column($items, 'slug');
+
+        return in_array($item['slug'], $somethingSlugsInCart);
+    }
+}
