4ee028a93bdd2daffb37e704917b1104573a6bd9
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-07-23 00:41:40 -0300

    Tela de Produtos

--- app/Http/Controllers/Front/StoreController.php
+++ app/Http/Controllers/Front/StoreController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Store;
 
 class StoreController extends Controller
 {
-    public function index($subdomain)
+    public function index($subdomain, Store $store)
     {
-        dd($subdomain);
+        $store = $store->whereSubdomain($subdomain)
+            ->with('products')->first();
+
+        return view('front.home', compact('store'));
     }
 }
