9908eece277e183edc67b44f1303c52a94854d8b
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-07-29 23:23:15 -0300

    Iniciando Melhorias Checkout

---
+++ app/Models/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\BelongsTenantScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory, BelongsTenantScope;
+
+    protected $fillable = [
+        'store_id', 'user_id', 'items', 'shipping_value', 'payment_status',
+    ];
+
+    public function setShippingValueAttribute($prop): void
+    {
+        $this->attributes['shipping_value'] = $prop * 100;
+    }
+
+    public function getShippingValueAttribute(): float|int
+    {
+        return $this->attributes['shipping_value'] / 100;
+    }
+
+    public function getItemsAttribute()
+    {
+        return unserialize($this->attributes['items']);
+    }
+
+    public function setItemsAttribute($prop): void
+    {
+        $this->attributes['items'] = serialize($prop);
+    }
+}
